cad93264b74b2b821ff6932218fe4374cbc25840
Martin Geno <geno@fireorbit.de>
2015-12-28 23:09:48 +0100

    web new event with old

--- static_src/js/app/new.js
+++ static_src/js/app/new.js
@@ -4,8 +4,9 @@ angular.module('monitormapApp')
 	.controller('NewCtrl', ['$scope','socket',function ($scope, socket) {
 		$scope.list = [];
 		socket.emit('node:list:new',function(result){
-			if(result.s)
+			if(result.s){
 				$scope.list = result.list;
+			}
 		});
 		socket.on('event::node:set:create',function(result){
 			$scope.list.push(result)
